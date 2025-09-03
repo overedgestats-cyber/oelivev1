cedf9af9e57c20e37fb4e4e326074c9b0657423e
unknown <overedgestats@gmail.com>
2025-09-03 18:50:00 +0300

    Remove vercel.json; pin functions to nodejs22.x; add root stubs for pages

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
 ﻿const app = require("../overedge-api/server");
 module.exports = (req, res) => app(req, res);
-module.exports.config = { runtime: "nodejs20.x" };
+module.exports.config = { runtime: "nodejs22.x" };
