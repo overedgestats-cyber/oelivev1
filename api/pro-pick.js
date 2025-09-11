f7f3abd4bd7d857f483189a698783828504fdb47
unknown <overedgestats@gmail.com>
2025-09-11 14:44:44 +0300

    Add Vercel forwarders for /api/hero-bet and /api/pro-pick

--- api/pro-pick.js
+++ api/pro-pick.js
@@ -1,3 +1,4 @@
-﻿const app = require("../overedge-api/server");
+﻿// Forward /api/pro-pick to the Express app
+const app = require("../overedge-api/server");
 module.exports = (req, res) => app(req, res);
 module.exports.config = { runtime: "nodejs20.x" };
